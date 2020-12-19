deef2ce8e5c7584aa70e318c4207014d3924a6f8
Daniele Stefenello <dstefene@redhat.com>
2020-12-18 22:19:09 -0300

    se elimina todo

--- app/anuncios/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'anuncios'
-urlpatterns = [
-    # post views
-    path('', views.anuncios_list, name='anuncios_list')
-]
--- app/anuncios/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Anuncio
-def anuncios_list(request):
-    anuncios = Anuncio.objects.filter(estado="published")
-    return render(request,
-                  'anuncios.html',
-                  {'anuncios': anuncios})
